16c77063abfafd03e88c92deccb3ccb4bf1af308
mcuong <huynhmc2804@gmail.com>
2025-04-11 14:26:54 +0700

    vi

--- app/src/main/java/com/example/app_dictionary_ev/Home.java
+++ app/src/main/java/com/example/app_dictionary_ev/Home.java
@@ -4,7 +4,12 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +17,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Home extends AppCompatActivity {
+    private EditText edtSearch;
+    private ImageView iconHeart;
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +52,38 @@ public class MainActivity extends AppCompatActivity {
                 return insets;
             });
         }
+
+        // Ánh xạ các thành phần
+        edtSearch = findViewById(R.id.edt_search);
+        iconHeart = findViewById(R.id.icon_heart);
+        listView = findViewById(R.id.listView);
+
+        // Ẩn danh sách ban đầu
+        //listView.setVisibility(View.GONE);
+
+        // Xóa nội dung khi nhấn ❌
+        iconHeart.setOnClickListener(v -> {
+            edtSearch.setText("");
+            edtSearch.clearFocus();
+            listView.setVisibility(View.GONE);
+        });
+
+        // Hiện danh sách khi nhập chữ
+        edtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0) {
+                    listView.setVisibility(View.VISIBLE);
+                } else {
+                    listView.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
     }
 }
